01bb3e1d95261c8ee505d1d9fd57954900c212f1
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-10-12 10:53:13 +0200

    commited solution

--- TarinYourProgrammerWFA/Form1.Designer.cs
+++ TarinYourProgrammerWFA/Form1.Designer.cs
@@ -24,13 +24,59 @@ namespace TarinYourProgrammerWFA {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.lbox_left = new System.Windows.Forms.ListBox();
+      this.lbox_right = new System.Windows.Forms.ListBox();
+      this.btn_move_to_right = new System.Windows.Forms.Button();
+      this.btn_move_to_left = new System.Windows.Forms.Button();
       this.SuspendLayout();
+      // 
+      // lbox_left
+      // 
+      this.lbox_left.FormattingEnabled = true;
+      this.lbox_left.Location = new System.Drawing.Point(12, 12);
+      this.lbox_left.Name = "lbox_left";
+      this.lbox_left.Size = new System.Drawing.Size(227, 251);
+      this.lbox_left.TabIndex = 0;
+      this.lbox_left.SelectedIndexChanged += new System.EventHandler(this.lbox_left_SelectedIndexChanged);
+      // 
+      // lbox_right
+      // 
+      this.lbox_right.FormattingEnabled = true;
+      this.lbox_right.Location = new System.Drawing.Point(326, 12);
+      this.lbox_right.Name = "lbox_right";
+      this.lbox_right.Size = new System.Drawing.Size(227, 251);
+      this.lbox_right.TabIndex = 1;
+      this.lbox_right.SelectedIndexChanged += new System.EventHandler(this.lbox_right_SelectedIndexChanged);
+      // 
+      // btn_move_to_right
+      // 
+      this.btn_move_to_right.Location = new System.Drawing.Point(245, 12);
+      this.btn_move_to_right.Name = "btn_move_to_right";
+      this.btn_move_to_right.Size = new System.Drawing.Size(75, 23);
+      this.btn_move_to_right.TabIndex = 2;
+      this.btn_move_to_right.Text = "--->";
+      this.btn_move_to_right.UseVisualStyleBackColor = true;
+      this.btn_move_to_right.Click += new System.EventHandler(this.btn_move_to_right_Click);
+      // 
+      // btn_move_to_left
+      // 
+      this.btn_move_to_left.Location = new System.Drawing.Point(245, 41);
+      this.btn_move_to_left.Name = "btn_move_to_left";
+      this.btn_move_to_left.Size = new System.Drawing.Size(75, 23);
+      this.btn_move_to_left.TabIndex = 3;
+      this.btn_move_to_left.Text = "<---";
+      this.btn_move_to_left.UseVisualStyleBackColor = true;
+      this.btn_move_to_left.Click += new System.EventHandler(this.btn_move_to_left_Click);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(564, 450);
+      this.Controls.Add(this.btn_move_to_left);
+      this.Controls.Add(this.btn_move_to_right);
+      this.Controls.Add(this.lbox_right);
+      this.Controls.Add(this.lbox_left);
       this.Name = "Form1";
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
@@ -39,6 +85,11 @@ namespace TarinYourProgrammerWFA {
     }
 
     #endregion
+
+    private System.Windows.Forms.ListBox lbox_left;
+    private System.Windows.Forms.ListBox lbox_right;
+    private System.Windows.Forms.Button btn_move_to_right;
+    private System.Windows.Forms.Button btn_move_to_left;
   }
 }
 
--- TarinYourProgrammerWFA/Form1.cs
+++ TarinYourProgrammerWFA/Form1.cs
@@ -10,12 +10,43 @@ using System.Windows.Forms;
 
 namespace TarinYourProgrammerWFA {
   public partial class Form1 : Form {
+    List<string> init_list_left, init_list_right;
     public Form1() {
       InitializeComponent();
+
+      init_list_left = new List<string>() { "Left1", "Left2", "Left3", "Left4", "Left5" };
+      init_list_right = new List<string>() { "Right1", "Right2", "Right3", "Right4", "Right5" };
+      btn_move_to_left.Enabled = lbox_right.SelectedIndex != -1;
+      btn_move_to_right.Enabled = lbox_left.SelectedIndex != -1;
+      lbox_left.SelectionMode = SelectionMode.MultiExtended;
+      lbox_right.SelectionMode = SelectionMode.MultiExtended;
+    }
+
+    private void lbox_left_SelectedIndexChanged(object sender, EventArgs e) {
+      btn_move_to_right.Enabled = lbox_left.SelectedIndex != -1;
+    }
+
+    private void lbox_right_SelectedIndexChanged(object sender, EventArgs e) {
+      btn_move_to_left.Enabled = lbox_right.SelectedIndex != -1;
+    }
+
+    private void btn_move_to_left_Click(object sender, EventArgs e) {
+      while(lbox_right.SelectedIndices.Count > 0) {
+        lbox_left.Items.Add(lbox_right.Items[lbox_right.SelectedIndices[0]]);
+        lbox_right.Items.RemoveAt(lbox_right.SelectedIndices[0]);
+      }
+    }
+
+    private void btn_move_to_right_Click(object sender, EventArgs e) {
+      while(lbox_left.SelectedIndices.Count > 0) {
+        lbox_right.Items.Add(lbox_left.Items[lbox_left.SelectedIndices[0]]);
+        lbox_left.Items.RemoveAt(lbox_left.SelectedIndices[0]);
+      }
     }
 
     private void Form1_Load(object sender, EventArgs e) {
-
+      lbox_left.Items.AddRange(init_list_left.ToArray());
+      lbox_right.Items.AddRange(init_list_right.ToArray());
     }
   }
 }
