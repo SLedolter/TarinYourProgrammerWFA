ef64ed94aa880e6001560a42f505c3c262e9dfc7
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-10-13 11:45:41 +0200

    SL: commited soluten

--- TarinYourProgrammerWFA/Form1.Designer.cs
+++ TarinYourProgrammerWFA/Form1.Designer.cs
@@ -24,21 +24,43 @@ namespace TarinYourProgrammerWFA {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+      this.textBox1 = new System.Windows.Forms.TextBox();
       this.SuspendLayout();
+      // 
+      // contextMenuStrip1
+      // 
+      this.contextMenuStrip1.Name = "contextMenuStrip1";
+      this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(12, 12);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 1;
+      this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+      this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.textBox1);
       this.Name = "Form1";
-      this.Text = "Form1";
+      this.Text = "No 8";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    private System.Windows.Forms.TextBox textBox1;
   }
 }
 
--- TarinYourProgrammerWFA/Form1.cs
+++ TarinYourProgrammerWFA/Form1.cs
@@ -17,5 +17,33 @@ namespace TarinYourProgrammerWFA {
     private void Form1_Load(object sender, EventArgs e) {
 
     }
+
+    private void textBox1_KeyPress(object sender, KeyPressEventArgs e) {
+      if(!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && !(e.KeyChar == '-' && textBox1.SelectionStart == 0) && !(e.KeyChar == '+' && textBox1.SelectionStart == 0)) {
+        e.Handled = true;
+      }
+    }
+
+    private void textBox1_KeyDown(object sender, KeyEventArgs e) {
+      int testVarInt32;
+      Int64 testVar;
+      switch(e.KeyCode) {
+        case Keys.Enter:
+          if(textBox1.Text == "") {
+            MessageBox.Show("Gib was ein!");
+          } else {
+            if(int.TryParse(textBox1.Text, out testVarInt32)) {
+              MessageBox.Show($"Die Zahl ist {textBox1.Text}!");
+            } else {
+              if(Int64.TryParse(textBox1.Text, out testVar)) {
+                MessageBox.Show($"Parsen fehlgeschlagen! Zu hoher Wertebereich!");
+              } else {
+                MessageBox.Show($"Diese Zahl ist weiter over 9000!");
+              }
+            }
+          }
+          break;
+      }
+    }
   }
 }
