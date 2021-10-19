05ddb370fb69a9d20834a5bb7bdacbfedbcc48f1
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-10-19 09:40:22 +0200

    SL: added calculation of single step new number series

--- TarinYourProgrammerWFA/Form1.Designer.cs
+++ TarinYourProgrammerWFA/Form1.Designer.cs
@@ -24,21 +24,91 @@ namespace TarinYourProgrammerWFA {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.inp_zahl = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.btn_calculate = new System.Windows.Forms.Button();
+      this.lbl_err_msg = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.lbl_output = new System.Windows.Forms.Label();
       this.SuspendLayout();
+      // 
+      // inp_zahl
+      // 
+      this.inp_zahl.Location = new System.Drawing.Point(56, 12);
+      this.inp_zahl.Name = "inp_zahl";
+      this.inp_zahl.Size = new System.Drawing.Size(100, 20);
+      this.inp_zahl.TabIndex = 0;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 15);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(31, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Zahl:";
+      // 
+      // btn_calculate
+      // 
+      this.btn_calculate.Location = new System.Drawing.Point(162, 10);
+      this.btn_calculate.Name = "btn_calculate";
+      this.btn_calculate.Size = new System.Drawing.Size(75, 23);
+      this.btn_calculate.TabIndex = 2;
+      this.btn_calculate.Text = "Berechnen";
+      this.btn_calculate.UseVisualStyleBackColor = true;
+      this.btn_calculate.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // lbl_err_msg
+      // 
+      this.lbl_err_msg.AutoSize = true;
+      this.lbl_err_msg.Location = new System.Drawing.Point(53, 35);
+      this.lbl_err_msg.Name = "lbl_err_msg";
+      this.lbl_err_msg.Size = new System.Drawing.Size(0, 13);
+      this.lbl_err_msg.TabIndex = 3;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(56, 65);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(102, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Berechnete Reihen:";
+      // 
+      // lbl_output
+      // 
+      this.lbl_output.AutoSize = true;
+      this.lbl_output.Location = new System.Drawing.Point(56, 90);
+      this.lbl_output.Name = "lbl_output";
+      this.lbl_output.Size = new System.Drawing.Size(0, 13);
+      this.lbl_output.TabIndex = 5;
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.lbl_output);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.lbl_err_msg);
+      this.Controls.Add(this.btn_calculate);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.inp_zahl);
       this.Name = "Form1";
       this.Text = "Form1";
-      this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.TextBox inp_zahl;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button btn_calculate;
+    private System.Windows.Forms.Label lbl_err_msg;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label lbl_output;
   }
 }
 
--- TarinYourProgrammerWFA/Form1.cs
+++ TarinYourProgrammerWFA/Form1.cs
@@ -10,12 +10,41 @@ using System.Windows.Forms;
 
 namespace TarinYourProgrammerWFA {
   public partial class Form1 : Form {
+    private long numberDigit;
+    private string workingDigit;
+
     public Form1() {
       InitializeComponent();
+
     }
 
-    private void Form1_Load(object sender, EventArgs e) {
+    private void button1_Click(object sender, EventArgs e) {
+      lbl_err_msg.Text = "";
+      if(!long.TryParse(inp_zahl.Text, out numberDigit) || numberDigit <= 0) {
+        lbl_err_msg.Text = "Bitte positive ganze Zahl eingeben!";
+      }
 
+      lbl_output.Text = CalculateNewNumberSeries(inp_zahl.Text);
+    }
+
+    private string CalculateNewNumberSeries(string inputDigit) {
+      string result = "";
+      char lastDigit;
+      int sameDigitCount = 1;
+
+      lastDigit = inputDigit[0];
+      for(int i = 1; i < inputDigit.Length; i++) {
+        if(lastDigit == inputDigit[i]) {
+          sameDigitCount++;
+        } else {
+          result += $"{sameDigitCount}{lastDigit}";
+          sameDigitCount = 1;
+          lastDigit = inputDigit[i];
+        }
+      }
+      result += $"{sameDigitCount}{lastDigit}";
+
+      return result;
     }
   }
 }
