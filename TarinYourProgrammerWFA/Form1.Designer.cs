07aeceb8cd3ff68b4b163abca5d8553579a0892e
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-10-20 14:22:37 +0200

    SL: added first sort method, should be optimized; added outputting to labels; added outputting cout to show how efficient it is

--- TarinYourProgrammerWFA/Form1.Designer.cs
+++ TarinYourProgrammerWFA/Form1.Designer.cs
@@ -24,21 +24,141 @@ namespace TarinYourProgrammerWFA {
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      this.lbl_array_in_label = new System.Windows.Forms.Label();
+      this.lbl_array_out_label = new System.Windows.Forms.Label();
+      this.lbl_array_out = new System.Windows.Forms.Label();
+      this.lbl_array_in = new System.Windows.Forms.Label();
+      this.btn_min_max = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.lbl_count = new System.Windows.Forms.Label();
+      this.btn_refill = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.lbl_min_max = new System.Windows.Forms.Label();
       this.SuspendLayout();
+      // 
+      // lbl_array_in_label
+      // 
+      this.lbl_array_in_label.AutoSize = true;
+      this.lbl_array_in_label.Location = new System.Drawing.Point(18, 13);
+      this.lbl_array_in_label.Name = "lbl_array_in_label";
+      this.lbl_array_in_label.Size = new System.Drawing.Size(46, 13);
+      this.lbl_array_in_label.TabIndex = 0;
+      this.lbl_array_in_label.Text = "In-Array:";
+      // 
+      // lbl_array_out_label
+      // 
+      this.lbl_array_out_label.AutoSize = true;
+      this.lbl_array_out_label.Location = new System.Drawing.Point(18, 63);
+      this.lbl_array_out_label.Name = "lbl_array_out_label";
+      this.lbl_array_out_label.Size = new System.Drawing.Size(54, 13);
+      this.lbl_array_out_label.TabIndex = 0;
+      this.lbl_array_out_label.Text = "Out-Array:";
+      // 
+      // lbl_array_out
+      // 
+      this.lbl_array_out.AutoSize = true;
+      this.lbl_array_out.Location = new System.Drawing.Point(76, 63);
+      this.lbl_array_out.Name = "lbl_array_out";
+      this.lbl_array_out.Size = new System.Drawing.Size(0, 13);
+      this.lbl_array_out.TabIndex = 1;
+      // 
+      // lbl_array_in
+      // 
+      this.lbl_array_in.AutoSize = true;
+      this.lbl_array_in.Location = new System.Drawing.Point(76, 13);
+      this.lbl_array_in.Name = "lbl_array_in";
+      this.lbl_array_in.Size = new System.Drawing.Size(0, 13);
+      this.lbl_array_in.TabIndex = 2;
+      // 
+      // btn_min_max
+      // 
+      this.btn_min_max.Location = new System.Drawing.Point(135, 33);
+      this.btn_min_max.Name = "btn_min_max";
+      this.btn_min_max.Size = new System.Drawing.Size(123, 23);
+      this.btn_min_max.TabIndex = 3;
+      this.btn_min_max.Text = "MinMax Calc";
+      this.btn_min_max.UseVisualStyleBackColor = true;
+      this.btn_min_max.Click += new System.EventHandler(this.btn_min_max_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(264, 38);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(90, 13);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Umschichtungen:";
+      // 
+      // lbl_count
+      // 
+      this.lbl_count.AutoSize = true;
+      this.lbl_count.Location = new System.Drawing.Point(360, 38);
+      this.lbl_count.Name = "lbl_count";
+      this.lbl_count.Size = new System.Drawing.Size(0, 13);
+      this.lbl_count.TabIndex = 5;
+      // 
+      // btn_refill
+      // 
+      this.btn_refill.Location = new System.Drawing.Point(14, 33);
+      this.btn_refill.Name = "btn_refill";
+      this.btn_refill.Size = new System.Drawing.Size(86, 23);
+      this.btn_refill.TabIndex = 6;
+      this.btn_refill.Text = "Refill Array";
+      this.btn_refill.UseVisualStyleBackColor = true;
+      this.btn_refill.Click += new System.EventHandler(this.btn_refill_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(21, 106);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(58, 13);
+      this.label2.TabIndex = 7;
+      this.label2.Text = "Min./Max.:";
+      // 
+      // lbl_min_max
+      // 
+      this.lbl_min_max.AutoSize = true;
+      this.lbl_min_max.Location = new System.Drawing.Point(85, 106);
+      this.lbl_min_max.Name = "lbl_min_max";
+      this.lbl_min_max.Size = new System.Drawing.Size(0, 13);
+      this.lbl_min_max.TabIndex = 8;
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.lbl_min_max);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.btn_refill);
+      this.Controls.Add(this.lbl_count);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.btn_min_max);
+      this.Controls.Add(this.lbl_array_out);
+      this.Controls.Add(this.lbl_array_in);
+      this.Controls.Add(this.lbl_array_out_label);
+      this.Controls.Add(this.lbl_array_in_label);
       this.Name = "Form1";
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
+
+    private System.Windows.Forms.Label lbl_array_in_label;
+    private System.Windows.Forms.Label lbl_array_out_label;
+    private System.Windows.Forms.Label lbl_array_out;
+    private System.Windows.Forms.Label lbl_array_in;
+    private System.Windows.Forms.Button btn_min_max;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label lbl_count;
+    private System.Windows.Forms.Button btn_refill;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label lbl_min_max;
   }
 }
 
--- TarinYourProgrammerWFA/Form1.cs
+++ TarinYourProgrammerWFA/Form1.cs
@@ -10,12 +10,85 @@ using System.Windows.Forms;
 
 namespace TarinYourProgrammerWFA {
   public partial class Form1 : Form {
+    int[] source_array;
+
     public Form1() {
       InitializeComponent();
     }
 
     private void Form1_Load(object sender, EventArgs e) {
 
+    }
+
+    protected override void OnLoad(EventArgs e) {
+      base.OnLoad(e);
+      source_array = FillArray(50);
+      OutputIntArrayToLabel(source_array, "lbl_array_in");
+    }
+
+    private void btn_min_max_Click(object sender, EventArgs e) {
+      int[] result_min_max_array, out_array;
+
+      out_array = SortIntegerArray1(source_array);
+      OutputIntArrayToLabel(out_array, "lbl_array_out");
+      lbl_min_max.Text = $"{out_array[0]}/{out_array[out_array.Length-1]}";
+    }
+
+    private int[] SortIntegerArray1(int[] in_array) {
+      int[] result_array = (int[])in_array.Clone();
+      int tempNumber, count = 0;
+
+      for(int i = 0; i < result_array.Length - 1; i++) {
+        if(result_array[i] <= result_array[i + 1]) {
+          continue;
+        } else {
+          for(int j = 1; j < result_array.Length; j++) {
+            if(result_array[j - 1] > result_array[j]) {
+              tempNumber = result_array[j - 1];
+              result_array[j - 1] = result_array[j];
+              result_array[j] = tempNumber;
+              i = 0;
+              count++;
+              break;
+            }
+          }
+        }
+      }
+
+      lbl_count.Text = count.ToString();
+      return result_array;
+    }
+
+    private void OutputIntArrayToLabel(int[] array_in, string label_name) {
+      Label output_label = (Label)this.Controls.Find(label_name, true)[0];
+      string output_text = "[";
+
+      for(int i = 0; i < array_in.Length; i++) {
+        output_text += array_in[i];
+        if(i < array_in.Length - 1) {
+          output_text += ",";
+        }
+      }
+
+      output_text += "]";
+
+      output_label.Text = output_text;
+    }
+
+    private int[] FillArray(int length) {
+      int[] resultArray = new int[length];
+      Random random = new Random();
+
+      for(int i=0; i<length; i++) {
+        resultArray[i] = random.Next(1, 1001);
+      }
+
+      return resultArray;
+    }
+
+    private void btn_refill_Click(object sender, EventArgs e) {
+      source_array = FillArray(50);
+      OutputIntArrayToLabel(source_array, "lbl_array_in");
     }
   }
 }
