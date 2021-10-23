9670124c3d123e80c73caac4f4773576034dd73e
Dean <dean.marinov@gmx.at>
2021-10-23 18:15:35 +0200

    fixed your code ;)

--- TarinYourProgrammerWFA/Form1.cs
+++ TarinYourProgrammerWFA/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 namespace TarinYourProgrammerWFA {
   public partial class Form1 : Form {
     int[] source_array;
+    int count = 0;
 
     public Form1() {
       InitializeComponent();
@@ -23,49 +24,47 @@ namespace TarinYourProgrammerWFA {
     protected override void OnLoad(EventArgs e) {
       base.OnLoad(e);
       source_array = FillArray(50);
-      OutputIntArrayToLabel(source_array, "lbl_array_in");
+      SortIntegerArray();
+      OutputIntArrayToLabel("lbl_array_in");
     }
 
-    private void btn_min_max_Click(object sender, EventArgs e) {
-      int[] result_min_max_array, out_array;
-
-      out_array = SortIntegerArray1(source_array);
-      OutputIntArrayToLabel(out_array, "lbl_array_out");
-      lbl_min_max.Text = $"{out_array[0]}/{out_array[out_array.Length-1]}";
+    private void Btn_min_max_Click(object sender, EventArgs e) {
+      OutputIntArrayToLabel("lbl_array_out");
+      lbl_min_max.Text = $"{source_array[0]} / {source_array[source_array.Length - 1]}";
     }
 
-    private int[] SortIntegerArray1(int[] in_array) {
-      int[] result_array = (int[])in_array.Clone();
-      int tempNumber, count = 0;
-
-      for(int i = 0; i < result_array.Length - 1; i++) {
-        if(result_array[i] <= result_array[i + 1]) {
-          continue;
-        } else {
-          for(int j = 1; j < result_array.Length; j++) {
-            if(result_array[j - 1] > result_array[j]) {
-              tempNumber = result_array[j - 1];
-              result_array[j - 1] = result_array[j];
-              result_array[j] = tempNumber;
-              i = 0;
-              count++;
-              break;
-            }
-          }
+    private void SortIntegerArray() {
+      int jumpindex;
+      count = 0;
+      for(int i = 0; i < source_array.GetLength(0) - 1; i++)
+      {
+        jumpindex = i;
+        while(source_array[i] > source_array[i + 1])
+        {
+          int _ = source_array[i];
+          source_array[i] = source_array[i+1];
+          source_array[i+1] = _;
+          count++;
+          if(i == 0)
+					{
+            break;
+					}
+          else
+					{
+            i--;
+					}
         }
+        i = jumpindex;
       }
-
       lbl_count.Text = count.ToString();
-      return result_array;
     }
-
-    private void OutputIntArrayToLabel(int[] array_in, string label_name) {
+    private void OutputIntArrayToLabel(string label_name) {
       Label output_label = (Label)this.Controls.Find(label_name, true)[0];
       string output_text = "[";
 
-      for(int i = 0; i < array_in.Length; i++) {
-        output_text += array_in[i];
-        if(i < array_in.Length - 1) {
+      for(int i = 0; i < source_array.GetLength(0); i++) {
+        output_text += source_array[i];
+        if(i < source_array.GetLength(0)) {
           output_text += ",";
         }
       }
@@ -76,19 +75,19 @@ namespace TarinYourProgrammerWFA {
     }
 
     private int[] FillArray(int length) {
-      int[] resultArray = new int[length];
+      source_array = new int[length];
       Random random = new Random();
 
-      for(int i=0; i<length; i++) {
-        resultArray[i] = random.Next(1, 1001);
+      for(int i=0; i<source_array.GetLength(0); i++) {
+        source_array[i] = random.Next(1, 1001);
       }
-
-      return resultArray;
+      return source_array;
     }
 
-    private void btn_refill_Click(object sender, EventArgs e) {
+    private void Btn_refill_Click(object sender, EventArgs e) {
       source_array = FillArray(50);
-      OutputIntArrayToLabel(source_array, "lbl_array_in");
+      SortIntegerArray();
+      OutputIntArrayToLabel("lbl_array_in");
     }
   }
 }
